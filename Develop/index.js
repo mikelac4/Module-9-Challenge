7566837d1398d230510724233ab82ba654753976
mikelac4 <112447725+mikelac4@users.noreply.github.com>
2023-01-25 16:34:21 -0500

    Started adding prompts with inquirer

--- Develop/index.js
+++ Develop/index.js
@@ -5,7 +5,46 @@ const inquirer = require('inquirer');
 const generateMarkdown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'title',
+            message: 'What is the title of your project?',   
+        },
+
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Give a description of your project.'
+        },
+
+        {
+            type: 'input',
+            name: 'installation',
+            message: 'What is required to get your project installed?'
+        },
+
+        {
+            type: 'input',
+            name: 'usage',
+            message: 'Provide instructions for use',
+        },
+
+        
+    ])
+
+    
+
+
+    
+
+    
+
+    
+
+
+
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
